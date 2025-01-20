11c2a74835d4225262a53adb9d7d0b7259786770
theJoshGG7 <80396766+theJoshGG7@users.noreply.github.com>
2025-01-20 20:06:27 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/auramation/', // Set the base URL for GitHub Pages
+  base: '/auramation/', // Base URL for GitHub Pages deployment
 });
